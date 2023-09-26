f356006e2d730cfef7b302cea21b391664585d21
Louis Alemar <louisalemar@gmail.com>
2023-09-26 13:56:08 -0400

    implement merge sort

--- algorithms/selectionSort/index.js
+++ algorithms/selectionSort/index.js
@@ -1,14 +1,17 @@
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
 function selectionSort(arr) {
-  for(var i = 0; i < arr.length; i++){
+  for (var i = 0; i < arr.length; i++) {
     var lowest = i;
 
-    for(var j = i + 1; j < arr.length; j++){
-      if(arr[j] < arr[lowest]){
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
         lowest = j;
       }
     }
-    
-    if(i !== lowest) {
+
+    if (i !== lowest) {
       // SWAP
       var temp = arr[i];
       arr[i] = arr[lowest];
@@ -18,4 +21,4 @@ function selectionSort(arr) {
   return arr;
 }
 
-console.log(selectionSort([0,2,34,22,10,19,17]))
+console.log(selectionSort([0, 2, 34, 22, 10, 19, 17]));
