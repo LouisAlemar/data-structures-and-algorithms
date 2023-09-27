e73133c7de2bc88689e52d5a59756e3a286a4a86
Louis Alemar <louisalemar@gmail.com>
2023-09-27 11:53:45 -0400

    implement pop method

--- data-structures/singlyLinkedList/index.js
+++ data-structures/singlyLinkedList/index.js
@@ -11,8 +11,10 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
   push(val) {
     let newNode = new Node(val);
+
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
@@ -21,6 +23,32 @@ class SinglyLinkedList {
       this.tail = newNode;
     }
     this.length++;
+
     return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
   }
 }
