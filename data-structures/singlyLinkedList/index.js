fb3d1e0f5787b471a341846863053c9e0c288ccc
Louis Alemar <louisalemar@gmail.com>
2023-09-27 12:05:29 -0400

    implement shift method

--- data-structures/singlyLinkedList/index.js
+++ data-structures/singlyLinkedList/index.js
@@ -51,4 +51,26 @@ class SinglyLinkedList {
 
     return current;
   }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return currentHead;
+  }
 }
+
+let list = new SinglyLinkedList();
+list.push("hello");
+list.push("world");
+list.push("test");
