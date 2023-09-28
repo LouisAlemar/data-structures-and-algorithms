6124a2f196953d33a95bbe333491ac53c0c1da75
Louis Alemar <louisalemar@gmail.com>
2023-09-28 10:56:12 -0400

    implement enqueue method

---
+++ data-structures/queue/index.js
@@ -0,0 +1,35 @@
+/**
+ * Name: Queue
+ * Time Complexity:
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // add node to end of queue
+  enqueue(val) {
+    let newNode = new Node(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+
+    return this.size;
+  }
+}
