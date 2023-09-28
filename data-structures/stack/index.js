b043a4944233007fd440cb9a5321b76b0b39bf73
Louis Alemar <louisalemar@gmail.com>
2023-09-28 10:25:06 -0400

    implement push method

---
+++ data-structures/stack/index.js
@@ -0,0 +1,38 @@
+/**
+ * Name: Stack
+ * Time Complexity:
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // add node to end of stack
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    this.size++;
+
+    return this.size;
+  }
+}
+
+let stack = new Stack();
