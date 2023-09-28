21b3d10a36733057b6adfc7b9fdd8b1097a8344a
Louis Alemar <louisalemar@gmail.com>
2023-09-28 16:19:39 -0400

    implement breadth first search

--- data-structures/binarySearchTree/index.js
+++ data-structures/binarySearchTree/index.js
@@ -95,4 +95,36 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  // breadth first search
+  breadthFirstSearch() {
+    let node = this.root;
+    let data = [];
+    let queue = [];
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    return data;
+  }
 }
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
