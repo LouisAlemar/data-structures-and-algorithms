36a67785c553d97552bef609a6bce53f03b12f1c
Louis Alemar <louisalemar@gmail.com>
2023-10-02 18:53:43 -0400

    implement remove edge method

--- data-structures/graph/index.js
+++ data-structures/graph/index.js
@@ -38,9 +38,21 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
+
+  // remove an edge from graph
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
 }
 
 let g = new Graph();
 g.addVertex("Tokyo");
 g.addVertex("Dallas");
 g.addVertex("Aspen");
+g.addEdge("Dallas", "Tokyo");
+g.addEdge("Dallas", "Aspen");
